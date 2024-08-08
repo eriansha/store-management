d8d6454bd7dba306380c9b759055ae597a5bafd5
Ivan Putra Eriansya <eriansha.van@gmail.com>
2024-08-08 22:24:34 +0700

    fix(store-web): layout navigation href

--- store-web/src/app/dashboard/layout.tsx
+++ store-web/src/app/dashboard/layout.tsx
@@ -10,10 +10,10 @@ export default function DashboardLayout({
       {children}
 
       <nav className='bg-gray-50 shadow-sm flex justify-evenly py-3'>
-        <Link href="dashboard">Dashboard</Link>
-        <Link href="dashboard/store">Store</Link>
-        <Link href="dashboard/transactions">Transaction</Link>
-        <Link href="dashboard/settlement">Settlement</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/dashboard/store">Store</Link>
+        <Link href="/dashboard/transactions">Transaction</Link>
+        <Link href="/dashboard/settlement">Settlement</Link>
       </nav>
     </section>
   )
