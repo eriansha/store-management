0e616c0916d511dd55bbeabfeed82272fed4e6dc
Ivan Putra Eriansya <eriansha.van@gmail.com>
2024-08-08 19:02:24 +0700

    feat(store-web): init button

---
+++ store-web/src/components/buttons/Buttons.tsx
@@ -0,0 +1,44 @@
+import { forwardRef } from 'react'
+
+type ButtonProps = {
+  className: string,
+  isLoading?: boolean,
+  disabled?: boolean,
+  pill?: boolean,
+} & React.ComponentPropsWithRef<'button'>;
+
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      className,
+      disabled: buttonDisabled,
+      isLoading,
+      pill,
+      ...props
+    },
+    ref
+  ) => {
+    const disabled = isLoading || buttonDisabled;
+    const borderRadius = pill ? "rounded-full" : 'rounded-md'
+    const ariaLabel = typeof children !== "string" ? "button label" : children
+
+    return (
+      <button
+        ref={ref}
+        type='button'
+        disabled={disabled}
+        className={`py-2 bg-green-600 ${borderRadius} text-white`}
+        aria-label={ariaLabel}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button"
+
+export default Button;
