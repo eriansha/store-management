8774f5e9925c6ed9897bfa3844a89f906eda6e99
Ivan Putra Eriansya <eriansha.van@gmail.com>
2024-08-08 23:18:59 +0700

    feat(store-web): init BottomNavigation
    - add heroicons package
    - create new component
    - remove unused layout

--- store-web/src/app/dashboard/home/layout.tsx
+++ store-web/src/app/dashboard/home/layout.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import BottomNavigation from '@/components/nav/BottomNavigation'
 
 export default function DashboardLayout({
   children,
@@ -6,17 +6,12 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <section className='relative'>
-      {children}
+    <section>
+      <main className='m-4'>
+        {children}
+      </main>
 
-      <div className='fixed bottom-0 w-full'>
-        <nav className='bg-gray-50 shadow-sm flex justify-evenly py-3'>
-          <Link href="/dashboard/home">Dashboard</Link>
-          <Link href="/dashboard/home/store">Store</Link>
-          <Link href="/dashboard/home/transactions">Transaction</Link>
-          <Link href="/dashboard/home/settlement">Settlement</Link>
-        </nav>
-      </div>
+      <BottomNavigation />
     </section>
   )
 }
---
+++ store-web/src/components/nav/BottomNavigation.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { usePathname } from 'next/navigation'
+import { HomeIcon, BuildingStorefrontIcon, PresentationChartLineIcon, WalletIcon, UserIcon } from '@heroicons/react/24/solid'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+
+const navs = [
+  {
+    href: "/dashboard/home",
+    Icon: HomeIcon
+  },
+  {
+    href: "/dashboard/home/store",
+    Icon: BuildingStorefrontIcon
+  },
+  {
+    href: "/dashboard/home/transactions",
+    Icon: PresentationChartLineIcon
+  },
+  {
+    href: "/dashboard/home/settlement",
+    Icon: WalletIcon
+  },
+  {
+    href: "/",
+    Icon: UserIcon
+  }
+]
+
+export default function BottomNavigation() {
+  const pathname = usePathname()
+
+  return (
+    <div className='fixed bottom-0 w-full'>
+      <nav className='bg-gray-50 shadow-sm flex justify-evenly py-3'>
+        {
+          navs.map((nav) => {
+            const { Icon } = nav
+
+            return (
+              <Link
+                key={nav.href}
+                href={nav.href}
+              >
+                <Icon
+                  className={cn(
+                    'size-6',
+                    'text-gray-500',
+                    [
+                      pathname === nav.href && [
+                        "text-green-500"
+                      ]
+                    ]
+                  )}
+                />
+              </Link>
+            )
+          })
+        }
+      </nav>
+    </div>
+  )
+}
