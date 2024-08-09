b0a9a57acae5491bb9e7f5976c7ca35a4bb19815
Ivan Putra Eriansya <eriansha.van@gmail.com>
2024-08-10 01:04:57 +0700

    feat(store-api): add merchant repo

---
+++ store-api/repositories/merchant/merchant_repository.go
@@ -0,0 +1,38 @@
+package merchant
+
+import (
+	"database/sql"
+	"log"
+)
+
+type MerchantRepository struct {
+	db *sql.DB
+}
+
+func NewMerchantRepository(db *sql.DB) *MerchantRepository {
+	return &MerchantRepository{db: db}
+}
+
+func (r *MerchantRepository) GetMerchant(ID int64) (*Merchant, error) {
+	var row *sql.Row
+	var err error
+
+	query := `SELECT
+		id,
+		name
+		FROM merchants
+		WHERE id = ?`
+	row = r.db.QueryRow(query, ID)
+
+	merchant := &Merchant{}
+	err = row.Scan(
+		&merchant.ID,
+		&merchant.Name)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return merchant, nil
+}
