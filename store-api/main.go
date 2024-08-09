c039b214f15050958419d11869444bf5832f2ea7
Ivan Putra Eriansya <eriansha.van@gmail.com>
2024-08-09 14:40:23 +0700

    ref(store-api): implement handlers
    - transaction handler
    - store handler

--- store-api/main.go
+++ store-api/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"store-api/handlers/store"
+	"store-api/handlers/transaction"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// define HTTP routes using the router
+	r.HandleFunc("/api/transaction-info", transaction.GetInfoHandler).Methods("GET")
+	r.HandleFunc("/api/stores", store.GetStoresHandler).Methods("GET")
+
+	log.Println("Server listening on :8090")
+	log.Fatal(http.ListenAndServe(":8090", r))
 }
