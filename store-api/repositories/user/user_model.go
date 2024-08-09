162c1471a3281087313c47bc2f81dd982190b619
Ivan Putra Eriansya <eriansha.van@gmail.com>
2024-08-10 01:05:12 +0700

    fix(store-api): change user.id type to int64

--- store-api/repositories/user/user_model.go
+++ store-api/repositories/user/user_model.go
@@ -1,7 +1,7 @@
 package user
 
 type User struct {
-	ID             int    `json:"id"`
+	ID             int64  `json:"id"`
 	MerchantID     string `json:"merchant_id"`
 	AccountNumber  string `json:"account_number"`
 	FullName       string `json:"full_name"`
